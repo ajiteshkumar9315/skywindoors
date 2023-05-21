0e08b360c1284b76bdb297f10b2d2ffe28ab834e
ajiteshkumar9315 <ajiteshkumar9315888@gmail.com>
2023-05-21 22:34:56 +0530

    adding the shrink animation in the product

--- src/Component/Product/Product.js
+++ src/Component/Product/Product.js
@@ -9,23 +9,23 @@ const Product = () => {
     <h2 className='head1'>OUR PRODUCTS</h2>
     <p className='para'>Skywin Windoors Is Leading System Aluminium Company In Delhi NCR We ProvideHigh End Product With Aesthetic Design and Finish.</p>
     <h1 className='head2'>SYSTEM ALUMINIUM</h1> 
-    <img src="../img2/4.jpg" alt="" className='img1' />
+    <img src="../img2/4.jpg" alt="" className='img1 imag' />
     <p className='p1'>Casement Windows</p>
-    <img src="../img2/5.jpg" alt="" className='img2' />
+    <img src="../img2/5.jpg" alt="" className='img2 imag' />
     <p className='p2'>Sliding windows</p>
-    <img src="../img2/6.jpg" alt="" className='img3' />
+    <img src="../img2/6.jpg" alt="" className='img3 imag' />
     <p className='p3'>Sliding Doors</p>
-    <img src="../img2/2.jpg" alt="" className='img4' />
+    <img src="../img2/2.jpg" alt="" className='img4 imag' />
     <p className='p4'>Casement Doors</p>
-    <img src="../img2/3.jpg" alt="" className='img5' />
+    <img src="../img2/3.jpg" alt="" className='img5 imag' />
     <p className='p5'>Fold And Slide</p>
-    <img src="../img2/1.jpg" alt="" className='img6' />
+    <img src="../img2/1.jpg" alt="" className='img6 imag' />
     <p className='p6'>Casment Aluminium Doors</p>
-    <img src="../img/11.png" alt="" className='img7' />
+    <img src="../img/11.png" alt="" className='img7 imag' />
     <p className='p7'>Tilt And Turn Windows</p>
-    <img src="../img/12.png" alt="" className='img8' />
+    <img src="../img/12.png" alt="" className='img8 imag' />
     <p className='p8'>Top Hung Windows</p>
-    <img src="../img/13.png" alt="" className='img13'/>
+    <img src="../img/13.png" alt="" className='img13 '/>
   
     </>
   )
